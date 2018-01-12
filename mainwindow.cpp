df9889afc8f8bcadd645a2ccf525d198846cd3a1
yang <283965457@qq.com>
2018-01-12 21:38:47 +0800

    second commit

---
+++ mainwindow.cpp
@@ -0,0 +1,24 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_textEdit_windowTitleChanged(const QString &title)
+{
+
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
